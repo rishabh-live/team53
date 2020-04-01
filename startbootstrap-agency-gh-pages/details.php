addacd5562d420f4de6de645b33c0d5fbbe3ef10
rishabharya1422 <rishabh12536@gmail.com>
2020-04-01 06:42:39 +0530

    added basic singuup form

--- startbootstrap-agency-gh-pages/details.php
+++ startbootstrap-agency-gh-pages/details.php
@@ -4,7 +4,33 @@ if (isset($_GET["city"])) {
   // code...
   $city = $_GET["city"];
 
-  echo $city;
-}
+  $session_id = rand(0,9999999999999).uniqid().rand(0,9999999999999);
 
+  //$_SESSION["id"] = $session_id;
+
+    if (isset($_GET["inuse"])) {
+      // code...
+      echo "<script>alert('Usernmae already in use') </script>";
+    }
+
+
+?>
+
+<form action="step-one.php" method="post">
+
+<div>
+  <label>Create Usernmae: </label>
+  <input type="text" name="username" autofocus="true" required="true">
+</div>
+
+<div>
+  <label>Choose Password: </label>
+  <input type="text" name="username" autofocus="true" required="true">
+</div>
+<input type="hidden" name="session_id" value="<?php echo $session_id; ?>">
+<input type="hidden" name="city" value="<?php echo $city; ?>">
+
+</form>
+<?php
+}
 ?>
